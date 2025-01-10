ff4deb23b0091b0c266387fb51c91a95d7cd4d48
Matyáš Brabec <matyas.brabec@seznam.cz>
2025-01-10 10:22:32 +0100

    implemented static lexicon dictionary

--- format.sh
+++ format.sh
@@ -6,5 +6,7 @@ find src/ \( -iname '*.hpp' -o -iname '*.cpp' -o -iname '*.cu' \) \
     ! -name 'gol_32_an5d_kernel.hu' \
     ! -name 'gol_64_an5d_host.cu' \
     ! -name 'gol_64_an5d_kernel.cu' \
-    ! -name 'gol_64_an5d_kernel.hu' -print0 \
+    ! -name 'gol_64_an5d_kernel.hu' \
+    ! -name 'patterns.hpp' \
+      -print0 \
     | xargs -0 clang-format -i
